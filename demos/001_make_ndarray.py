0c76762b3dc76c2c9d5b7feba18fc0698b8f2634
wb-lyf573387 <864938527@qq.com>
2020-01-13 14:37:04 +0800

    转换数据类型

--- demos/001_make_ndarray.py
+++ demos/001_make_ndarray.py
@@ -3,8 +3,8 @@
 """
 import numpy as np
 
-data01 = [6, 7.5, 0, 9, 3]
-arr01 = np.array(data01, dtype=np.int32)  # 指定数据类型
+data01 = [6, 7.5, -2.1, 9, 3]
+arr01 = np.array(data01, dtype=np.float64)  # 指定数据类型
 
 data02 = [[1, 2, 3, 4], [5, 6, 7, 8]]
 arr02 = np.array(data02)
@@ -15,7 +15,9 @@ arr04 = np.ones((3, 4))
 
 arr05 = np.ones_like(arr01)
 
-arr06 = arr01.astype(np.float64)  # 转换数据类型
+arr06 = arr01.astype(np.int32)  # 转换数据类型
+
+arr07 = arr01.astype(np.uint8)  # 转换数据类型
 
 print("arr01\n", arr01)
 print("arr02\n", arr02)
@@ -23,3 +25,4 @@ print("arr03\n", arr03)
 print("arr04\n", arr04)
 print("arr05\n", arr05)
 print("arr06\n", arr06)
+print("arr07\n", arr07)
